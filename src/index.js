8ddd21c082a877aae81cfc13ffaaca6280435e5f
penevl <103186686+penevl@users.noreply.github.com>
2022-08-20 02:07:41 +0300

    Added propper logging

--- src/index.js
+++ src/index.js
@@ -1,131 +1,196 @@
-const gamedig = require("gamedig")
+import gamedig from"gamedig"
 
-const dotenv = require("dotenv");
-const { google } = require("googleapis");
+import dotenv from"dotenv"
+dotenv.config()
 
-var moment = require('moment')
+import { google } from"googleapis" 
 
-dotenv.config();
+import moment from'moment'
+import logger from"./logger.js"
 
-console.log("Reading SERVER_IP")
-const SERVER_IP = process.env.SERVER_IP;
-console.log("Server IP read successfuly: " + SERVER_IP)
+logger.setLoggingLevel(process.env.LOG_LEVEL);
+logger.setLogFileLocation(process.env.LOG_LOCATION)
 
-console.log("Reading spreadsheetId")
-const spreadsheetId = process.env.SPREADSHEET_ID;
-console.log("Spreadsheet id read successfuly: " + spreadsheetId)
+ 
 
-console.log("Reading alpha members")
-const ALPHA = process.env.ALPHA.split(",");
+logger.info("Reading SERVER_IP")
+const SERVER_IP = process.env.SERVER_IP 
+logger.info("Server IP read successfuly")
+logger.debug("Server ip is: " + SERVER_IP)
+
+logger.info("Reading spreadsheetId")
+const spreadsheetId = process.env.SPREADSHEET_ID 
+logger.info("Spreadsheet id read successfuly")
+logger.debug("Spreadsheet id is: "  + spreadsheetId)
+
+logger.info("Reading alpha members")
+const ALPHA = process.env.ALPHA.split(",") 
 var AlphaAttended = []
-console.log("Alpha members read successfuly: " + ALPHA)
+logger.info("Alpha members read successfuly")
+logger.debug("Alpha members are: "  + ALPHA)
 
-console.log("Reading bravo members")
-const BRAVO = process.env.BRAVO.split(",");
+logger.info("Reading bravo members")
+const BRAVO = process.env.BRAVO.split(",") 
 var BravoAttended = []
-console.log("Bravo members read successfuly: " + BRAVO)
+logger.info("Bravo members read successfuly")
+logger.debug("Bravo members are: " + BRAVO)
 
-console.log("Reading charlie members")
-const CHARLIE = process.env.CHARLIE.split(",");
+logger.info("Reading charlie members")
+const CHARLIE = process.env.CHARLIE.split(",") 
 var CharlieAttended = []
-console.log("Charlie members read successfuly: " + CHARLIE)
+logger.info("Charlie members read successfuly")
+logger.debug("Charlie members are: "  + CHARLIE)
 
-console.log("Reading delta members")
-const DELTA = process.env.DELTA.split(",");
-var DeltaAttended = [];
-console.log("Delta members read successfuly: " + DELTA)
+logger.info("Reading delta members")
+const DELTA = process.env.DELTA.split(",") 
+var DeltaAttended = [] 
+logger.info("Delta members read successfuly")
+logger.debug("Delta members are: "  + DELTA)
 
-var playerList = [];
-const date = new Date();
-// var dateTime = date.getUTCDay() + "/" + date.getUTCMonth() + "/" + date.getFullYear()
+var playerList = [] 
+const date = new Date() 
 var dateTime = moment(date, "YYYY-MM-DD").add(new Date().getTimezoneOffset(),'minute').format('DD-MM-YYYY')
 
+logger.info("Creating google auth object")
 const auth = new google.auth.GoogleAuth({
     keyFile : "credentials.json",
     scopes : "https://www.googleapis.com/auth/spreadsheets"
 })
+logger.info("Creating google auth object successful")
 
-const client = auth.getClient();
+logger.info("Creating google client object")
+const client = auth.getClient() 
+logger.info("Creating google client object successful")
 
+logger.info("Creating googleSheets object")
 const googleSheets = google.sheets({
     version : "v4",
     auth: client
 })
+logger.info("googleSheets object created successfuly")
 
-console.log("Quering server")
+logger.info("Quering server")
 gamedig.query({
     type: 'arma3',
     host: SERVER_IP
 }).then((state) => {
-    console.log("Quering successful\nGetting players")
+    logger.info("Quering successful")
+    logger.info("Getting players")
     state.players.forEach(p => {
         var player = p.name
         playerList.push(player)
+        logger.debug("Player " + player +" added to playerList")
+        logger.debug("Player " + player +" added to ")
         if (ALPHA.includes(player)){
-            AlphaAttended.push(player)        
+            AlphaAttended.push(player)
+            logger.debug("Player " + player +" added to ALPHA")        
         }
         if (BRAVO.includes(player)){
             BravoAttended.push(player)
+            logger.debug("Player " + player +" added to BRAVO")
         }
         if (CHARLIE.includes(player)){
             CharlieAttended.push(player)
+            logger.debug("Player " + player +" added to CHARLIE")
         }
         if (DELTA.includes(player)){
             DeltaAttended.push(player)
+            logger.debug("Player " + player +" added to DELTA")
         }
     })
-    console.log("Players gotten successfuly: " + playerList)
-   
-    googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "All!A:B",
-        valueInputOption: "RAW",
-        resource: {
-          values: [[dateTime, playerList.toString()]],
-        },
-      });
+    logger.info("Players gotten successfuly")
 
-      googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Alpha!A:B",
-        valueInputOption: "RAW",
-        resource: {
-          values: [[dateTime, AlphaAttended.toString()]],
-        },
-      });
+    logger.debug("Players are: "  + playerList)
+    logger.debug("AlphaAttended is: " + AlphaAttended)
+    logger.debug("BravoAttended is: " + BravoAttended)
+    logger.debug("CharlieAttended is: " + CharlieAttended)
+    logger.debug("DeltaAttended is: " + DeltaAttended)
 
-      googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Bravo!A:B",
-        valueInputOption: "RAW",
-        resource: {
-          values: [[dateTime, BravoAttended.toString()]],
-        },
-      });
-
-      googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Charlie!A:B",
-        valueInputOption: "RAW",
-        resource: {
-          values: [[dateTime, CharlieAttended.toString()]],
-        },
-      });
-
-      googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Delta!A:B",
-        valueInputOption: "RAW",
-        resource: {
-          values: [[dateTime, DeltaAttended.toString()]],
-        },
-      });
-
+    logAll();
+      
 }).catch((error) => {
-    console.log(error)
-});
+    logger.info(error)
+}) 
+
+function logAll(){
+  logEveryone()
+  logAlpha()
+  logBravo()
+  logCharlie()
+  logDelta()
+}
+
+function logEveryone(){
+
+  googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "All!A:B",
+    valueInputOption: "RAW",
+    resource: {
+      values: [[dateTime, playerList.toString()]],
+    },
+  })
+  logger.info("All players logged to google sheets")
+
+}
+
+function logAlpha(){
+
+  googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Alpha!A:B",
+    valueInputOption: "RAW",
+    resource: {
+      values: [[dateTime, AlphaAttended.toString()]],
+    },
+  }) 
+  logger.info("All players from AlphaAttended logged to google sheets")
+
+}
+
+function logBravo(){
+
+  googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Bravo!A:B",
+    valueInputOption: "RAW",
+    resource: {
+      values: [[dateTime, BravoAttended.toString()]],
+    },
+  })
+  logger.info("All players from BravoAttended logged to google sheets")
+
+}
+
+function logCharlie(){
+
+  googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Charlie!A:B",
+    valueInputOption: "RAW",
+    resource: {
+      values: [[dateTime, CharlieAttended.toString()]],
+    },
+  })
+  logger.info("All players from CharlieAttended logged to google sheets")
+
+}
+
+function logDelta(){
+
+  googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Delta!A:B",
+    valueInputOption: "RAW",
+    resource: {
+      values: [[dateTime, DeltaAttended.toString()]],
+    },
+  }) 
+  logger.info("All players from DeltaAttended logged to google sheets")
+
+}
---
+++ src/logger.js
@@ -0,0 +1,92 @@
+import fs from "fs"
+import moment from'moment'
+
+    var logDebug = false
+    var logInfo = true
+    var logWarning = true
+    var logError = true
+    var loggingEnabled = false
+    var fileLocation = ""
+    var fileName = getTime()
+    const setLogFileLocation = (location) => { 
+        fileLocation = location + "/"
+        loggingEnabled = true
+    }
+    
+    const setLoggingLevel = (level) =>{
+        switch (level) {
+            case "debug":
+                logDebug = true
+                logInfo = true
+                logWarning = true
+                logError = true
+                break;
+
+            case "info":
+                logDebug = false
+                logInfo = true
+                logWarning = true
+                logError = true
+                break;
+
+            case "warn":
+                logDebug = false
+                logInfo = false
+                logWarning = true
+                logError = true
+                break;
+            case "error":
+                logDebug = false
+                logInfo = false
+                logWarning = false
+                logError = true
+                break;    
+            default:
+                logDebug = false
+                logInfo = true
+                logWarning = true
+                logError = true
+                break;
+        }
+    }
+    
+    const debug = (message) => {
+        if(logDebug && loggingEnabled){
+        console.log(getTime() + "    [DEBUG] " + message)
+        fs.appendFile( "logs/" + fileName + ".log",getTime() + "    [DEBUG] " + message + "\n", function (err) {
+            if (err) return console.log(err);
+          });
+        }
+    }
+
+    const info = (message) => {
+        if(logInfo && loggingEnabled){
+        console.log(getTime() +"    [INFO] " + message)
+        fs.appendFile( "logs/" + fileName + ".log",getTime() +"    [INFO] " + message + "\n", function (err) {
+            if (err) return console.log(err);
+          });
+        }
+    }
+
+    const warn = (message) => {
+        if(logWarning && loggingEnabled){
+        console.warn(getTime() + "    [WARNING] " + message)
+        fs.appendFile( "logs/" + fileName + ".log",getTime() + "    [WARNING] " + message + "\n", function (err) {
+            if (err) return console.log(err);
+          });
+        }
+    }
+
+    const error = (message) => {
+        if(logError && loggingEnabled){
+        console.log(getTime() + "    [ERROR] " + message)
+        fs.appendFile( "logs/" + fileName + ".log",getTime() + "    [ERROR] " + message + "\n", function (err) {
+            if (err) return console.log(err);
+          });
+        }  
+    }
+
+function getTime(){
+    return moment(new Date(), "YYYY-MM-DD").add(new Date().getTimezoneOffset(),'minute').format('DD-MM-YYYY-H-m-s')
+}    
+export default {setLogFileLocation,setLoggingLevel,debug,info,warn,error}
